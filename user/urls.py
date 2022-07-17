75cc18ba034d33f0d5aff7a997fe3df88d9ee30b
willy-10225 <swilly10225@gmail.com>
2022-07-17 11:45:56 +0800

    password

--- user/urls.py
+++ user/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/',views.user_register)
+    path('',views.user_register)
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 # Create your views here.
-def user_register(reguest):
-    return render(reguest,'./user/register.html')
+
+
+def user_register(request):
+    message = ''
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        print(request.POST)
+        username = request.POST.get('username')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+        print(username, password1, password2)
+
+        if password1 != password2:
+            message = '兩次密碼不同'
+        elif len(password1) < 8:
+            message = '密碼過短(至少八個字)'
+
+        else:
+            # 使用者名稱是否重複
+            if User.objects.filter(username=username).exists():
+                message = '帳號重複'
+            else:
+                user = User.objects.create_user(
+                    username=username, password=password1)
+                if user:
+                    user.save()
+                    message = '註冊成功!'
+
+    return render(request, './user/register.html', {'form': form, 'message': message})
